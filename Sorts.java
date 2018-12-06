7d8ba240de18b4f025b9ad4af76158ca62db5b0e
sxiao1 <sxiao1@stuy.edu>
2018-12-05 19:26:49 -0500

    tested

--- Sorts.java
+++ Sorts.java
@@ -16,8 +16,70 @@ public static String printData(int[] data){
   String newstr = "[";
   for(int i = 0; i < data.length; i ++){
     newstr += data[i];
+    if(i+ 1 != data.length){
+      newstr += ", ";
+    }
   }
   newstr+= "]";
   return newstr;
 }
+public static void main(String[] args) {
+
+    // Google random int generator
+
+    String sorted = "";
+
+    // 8 nums, [0:10]
+    int[] test_a = { 3, 4, 7, 1, 6, 2, 8, 6 };
+    System.out.println(printData(test_a));
+    // [3, 4, 7, 1, 6, 2, 8, 6]
+
+    selectionSort(test_a);
+
+    sorted = printData(test_a);
+    System.out.println(sorted);
+    // [1, 2, 3, 4, 6, 6, 7, 8]
+
+    if(sorted.equals("[1, 2, 3, 4, 6, 6, 7, 8]")) {
+      System.out.println("TEST CASE - PASS");
+    } else {
+      System.out.println("TEST CASE - FAIL");
+    }
+
+    // 12 nums, [0:20]
+    int[] test_b = { 17, 1, 19, 2, 18, 20, 1, 18, 11, 13, 5, 17 };
+    System.out.println(printData(test_b));
+    // [17, 1, 19, 2, 18, 20, 1, 18, 11, 13, 5, 17]
+
+    selectionSort(test_b);
+
+    sorted = printData(test_b);
+    System.out.println(sorted);
+    // [1, 1, 2, 5, 11, 13, 17, 17, 18, 18, 19, 20]
+
+    if(sorted.equals("[1, 1, 2, 5, 11, 13, 17, 17, 18, 18, 19, 20]")) {
+      System.out.println("TEST CASE - PASS");
+    } else {
+      System.out.println("TEST CASE - FAIL");
+    }
+
+    // 20 nums, [0:100]
+    int[] test_c = { 14, 32, 19, 66, 61, 96, 33, 48, 30, 100, 65, 37, 15, 85, 1, 35, 9, 57, 50, 52 };
+    System.out.println(printData(test_c));
+    // [14, 32, 19, 66, 61, 96, 33, 48, 30, 100, 65, 37, 15, 85, 1, 35, 9, 57, 50, 52]
+
+    selectionSort(test_c);
+
+    sorted = printData(test_c);
+    System.out.println(sorted);
+    // [1, 9, 14, 15, 19, 30, 32, 33, 35, 37, 48, 50, 52, 57, 61, 65, 66, 85, 96, 100]
+
+    if(sorted.equals("[1, 9, 14, 15, 19, 30, 32, 33, 35, 37, 48, 50, 52, 57, 61, 65, 66, 85, 96, 100]")) {
+      System.out.println("TEST CASE - PASS");
+    } else {
+      System.out.println("TEST CASE - FAIL");
+    }
+
+  }
+
 }
